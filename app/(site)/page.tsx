e2d3023b00003f6af86597ffedf30eec7da4170b
pavelhaj93 <pavel.hajduch@actumdigital.com>
2024-02-19 11:38:41 +0700

    db test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,28 @@
 // This is a homepage
+import prisma from "@/app/libs/prismadb";
 
 import styles from "./page.module.css";
 
+async function fetchBottles() {
+  const bottles = await prisma.bottle.findMany();
+
+  return bottles;
+}
+
 export default async function Home() {
+  const bottles = await fetchBottles();
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>
         This is your Homepage, a server component
       </h1>
+      <div>
+        {bottles.map((bottle) => (
+          <div key={bottle.id}>
+            <p>{bottle.code}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
